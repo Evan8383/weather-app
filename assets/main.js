05e0461429ca1db7013b62459099db50a21eb657
evan8383 <evanbennett83@gmai.com>
2023-10-20 07:58:28 -0400

    Completed styling for the today weather section. Started styling on the 5 day forecast section.

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,30 @@ const searchBar = document.querySelector('#searchBar')
 const appContainer = document.querySelector("body > main")
 const form = document.querySelector("body > main > section > form")
 
+let mouseDown = false;
+let startX, scrollLeft;
+const slider = document.querySelector('#slider');
+
+const startDragging = (e) => {
+  mouseDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+}
+
+const stopDragging = (e) => {
+  mouseDown = false;
+}
+
+const move = (e) => {
+  e.preventDefault();
+  if(!mouseDown) { return; }
+  const x = e.pageX - slider.offsetLeft;
+  const scroll = x - startX;
+  slider.scrollLeft = scrollLeft - scroll;
+}
+
+// Add the event listeners
+slider.addEventListener('mousemove', move, false);
+slider.addEventListener('mousedown', startDragging, false);
+slider.addEventListener('mouseup', stopDragging, false);
+slider.addEventListener('mouseleave', stopDragging, false);
