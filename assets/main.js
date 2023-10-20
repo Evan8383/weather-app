986ee47371d7b0f7419483b24005acc006ec417b
evan8383 <evanbennett83@gmai.com>
2023-10-19 22:51:35 -0400

    Adjusted results layout.

--- assets/main.js
+++ assets/main.js
@@ -7,17 +7,3 @@ const searchBar = document.querySelector('#searchBar')
 const appContainer = document.querySelector("body > main")
 const form = document.querySelector("body > main > section > form")
 
-form.addEventListener('submit', (e)=>{
-  e.preventDefault()
-  // e.stopImmediatePropagation()
-    appContainer.setAttribute('data-status', 'loading')
-    searchBar.value = ''
-    const test = setInterval(()=>{
-      appContainer.setAttribute('data-status', 'results')
-      clearInterval(test)
-    }, 1000, {once: true})
-})
-
-results.addEventListener('click', ()=>{
-  appContainer.setAttribute('data-status', '')
-})
