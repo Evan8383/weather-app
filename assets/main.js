f2837ff6e6247d8a73dd8d2d43d50f6e4908f31b
evan8383 <evanbennett83@gmai.com>
2023-10-22 12:40:09 -0400

    Added functionality for clicking and searching past searches.

--- assets/main.js
+++ assets/main.js
@@ -24,175 +24,116 @@ let searchedValue
 
 searchBtn.addEventListener('click', (event) => {
   event.preventDefault()
-  recentSearchBtn.disable
+
+  searchedValue = `https://api.openweathermap.org/data/2.5/weather?q=${searchBar.value},&appid=548a7af7dc28b5422813335d1da2e872&units=imperial`
+  
   addRecentSearch() // adds the appropriate html elements to display the recent search
   saveLocalStorage() // saves the inner HTML to local storage
 
   loader.classList.remove('hidden')
-  const geoUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchBar.value},&appid=548a7af7dc28b5422813335d1da2e872&units=imperial`
-  searchBar.value = ''
 
   todayDay.textContent = getTodayDay
   todayDate.textContent = getTodayDate
 
-  getWeather(geoUrl)
+  getWeather(searchedValue)
+  recentSearchList.classList.remove('active')
+  searchBar.value = ''
+  searchedValue = ''
 })
 
-function getWeather(geoUrl){
-  fetch(geoUrl)
-  .then(res => res.json())
-  .then(data => {
-    let weatherIcon = data.weather[0].icon
-    const iconUrl = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`
-    largeIcon.setAttribute('src', iconUrl)
 
-    currentCity.textContent = data.name
-    todayTemp.innerHTML = `${Math.floor(data.main.temp)} <span>&#176;</span>`
-    todayWind.innerHTML = `<i class="fa-solid fa-wind"></i> ${Math.floor(data.wind.speed)}mph`
-    todayHumid.innerHTML = `RH: ${Math.floor(data.main.humidity)} <i class="fa-solid fa-percent"></i>`
+recentSearchList.addEventListener('click', (e) => {
+  if (e.target.nodeName === 'SPAN') {
+    e.target.parentElement.remove()
+    saveLocalStorage()
+  } else {
+    loader.classList.remove('hidden')
+    
+    searchedValue = `https://api.openweathermap.org/data/2.5/weather?q=${e.target.textContent.slice(0, -1)},&appid=548a7af7dc28b5422813335d1da2e872&units=imperial`
+    getWeather(searchedValue)
 
-    getFiveDayForecast(data.coord.lat, data.coord.lon)
-  })
-}
-
-function getFiveDayForecast(lat, lon){
-  const coordUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=548a7af7dc28b5422813335d1da2e872&units=imperial`
-
-      fetch(coordUrl)
-        .then(res => res.json())
-        .then(data => {
-          let fiveDayTemp = []
-          let fiveDayIcon = []
-          let fiveDayDate = []
-          let fiveDayWind = []
-          let fiveDayHumid = []
-
-          const futureTemp = document.querySelectorAll('.future-temp')
-          const futureIcon = document.querySelectorAll('.weather-sub-card img')
-          const futureDate = document.querySelectorAll('.future-date')
-          const futureWind = document.querySelectorAll('.future-wind')
-          const futureHumid = document.querySelectorAll('.future-humid')
-
-          data.list.forEach(item => {
-            const daysDate = new Date(item.dt * 1000)
-            if (daysDate.getHours() === 11) {
-              fiveDayTemp.push(item.main.temp)
-              let fiveDayIconUrl = `https://openweathermap.org/img/wn/${item.weather[0].icon}.png`
-              fiveDayIcon.push(fiveDayIconUrl)
-              fiveDayWind.push(item.wind.speed)
-              fiveDayHumid.push(item.main.humidity)
-            }
-          })
-          futureTemp.forEach((temp, i) => {
-            temp.innerHTML = `${Math.floor(fiveDayTemp[i])} <span>&#176;</span>`
-          })
-          futureIcon.forEach((icon, i) => {
-            icon.setAttribute('src', fiveDayIcon[i])
-          })
-          for (let i = 0; i < 5; i++) {
-            const nextDate = currentDate.add(i + 1, 'day');
-            fiveDayDate.push(nextDate.format('ddd MM/DD'));
-          }
-          futureDate.forEach((day, i) => {
-            day.textContent = fiveDayDate[i]
-          })
-          futureWind.forEach((value, i) => {
-            value.innerHTML = `<i class="fa-solid fa-wind"></i> ${Math.floor(fiveDayWind[i])} mph`
-          })
-          futureHumid.forEach((value, i) => {
-            value.innerHTML = `RH: ${Math.floor(fiveDayHumid[i])} <i class="fa-solid fa-percent"></i> `
-          })
-          recentSearchBtn.enable
-          loader.classList.add('hidden')
-          resultSection.classList.remove('hidden')
-        })
-    }
-
-  // fetch(geoUrl)
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     let weatherIcon = data.weather[0].icon
-  //     const iconUrl = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`
-  //     largeIcon.setAttribute('src', iconUrl)
-
-  //     currentCity.textContent = data.name
-  //     todayTemp.innerHTML = `${Math.floor(data.main.temp)} <span>&#176;</span>`
-  //     todayWind.innerHTML = `<i class="fa-solid fa-wind"></i> ${Math.floor(data.wind.speed)}mph`
-  //     todayHumid.innerHTML = `RH: ${Math.floor(data.main.humidity)} <i class="fa-solid fa-percent"></i>`
-
-//       const lat = data.coord.lat
-//       const lon = data.coord.lon
-//       const coordUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=548a7af7dc28b5422813335d1da2e872&units=imperial`
-
-//       fetch(coordUrl)
-//         .then(res => res.json())
-//         .then(data => {
-//           let fiveDayTemp = []
-//           let fiveDayIcon = []
-//           let fiveDayDate = []
-//           let fiveDayWind = []
-//           let fiveDayHumid = []
-
-//           const futureTemp = document.querySelectorAll('.future-temp')
-//           const futureIcon = document.querySelectorAll('.weather-sub-card img')
-//           const futureDate = document.querySelectorAll('.future-date')
-//           const futureWind = document.querySelectorAll('.future-wind')
-//           const futureHumid = document.querySelectorAll('.future-humid')
-
-//           data.list.forEach(item => {
-//             const daysDate = new Date(item.dt * 1000)
-//             if (daysDate.getHours() === 11) {
-//               fiveDayTemp.push(item.main.temp)
-//               let fiveDayIconUrl = `https://openweathermap.org/img/wn/${item.weather[0].icon}.png`
-//               fiveDayIcon.push(fiveDayIconUrl)
-//               fiveDayWind.push(item.wind.speed)
-//               fiveDayHumid.push(item.main.humidity)
-//             }
-//           })
-//           futureTemp.forEach((temp, i) => {
-//             temp.innerHTML = `${Math.floor(fiveDayTemp[i])} <span>&#176;</span>`
-//           })
-//           futureIcon.forEach((icon, i) => {
-//             icon.setAttribute('src', fiveDayIcon[i])
-//           })
-//           for (let i = 0; i < 5; i++) {
-//             const nextDate = currentDate.add(i + 1, 'day');
-//             fiveDayDate.push(nextDate.format('ddd MM/DD'));
-//           }
-//           futureDate.forEach((day, i) => {
-//             day.textContent = fiveDayDate[i]
-//           })
-//           futureWind.forEach((value, i) => {
-//             value.innerHTML = `<i class="fa-solid fa-wind"></i> ${Math.floor(fiveDayWind[i])} mph`
-//           })
-//           futureHumid.forEach((value, i) => {
-//             value.innerHTML = `RH: ${Math.floor(fiveDayHumid[i])} <i class="fa-solid fa-percent"></i> `
-//           })
-//           recentSearchBtn.enable
-//           loader.classList.add('hidden')
-//           resultSection.classList.remove('hidden')
-//         })
-//     })
-// })
-
-// const searchHistory = localStorage.getItem('recentSearches')
-// recentSearchList.innerHTML = searchHistory
-
-
-recentSearchList.addEventListener('click', removeRecentSearch)
+    searchedValue = ''
+    recentSearchList.classList.remove('active')
+  }
+})
 
 recentSearchBtn.addEventListener('click', toggleSearchMenu)
+
+function getWeather(url) {
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      let weatherIcon = data.weather[0].icon
+      const iconUrl = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+      largeIcon.setAttribute('src', iconUrl)
+
+      currentCity.textContent = data.name
+      todayTemp.innerHTML = `${Math.floor(data.main.temp)} <span>&#176;</span>`
+      todayWind.innerHTML = `<i class="fa-solid fa-wind"></i> ${Math.floor(data.wind.speed)}mph`
+      todayHumid.innerHTML = `RH: ${Math.floor(data.main.humidity)} <i class="fa-solid fa-percent"></i>`
+
+      getFiveDayForecast(data.coord.lat, data.coord.lon)
+    })
+}
+
+function getFiveDayForecast(lat, lon) {
+  const coordUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=548a7af7dc28b5422813335d1da2e872&units=imperial`
+
+  fetch(coordUrl)
+    .then(res => res.json())
+    .then(data => {
+      let fiveDayTemp = []
+      let fiveDayIcon = []
+      let fiveDayDate = []
+      let fiveDayWind = []
+      let fiveDayHumid = []
+
+      const futureTemp = document.querySelectorAll('.future-temp')
+      const futureIcon = document.querySelectorAll('.weather-sub-card img')
+      const futureDate = document.querySelectorAll('.future-date')
+      const futureWind = document.querySelectorAll('.future-wind')
+      const futureHumid = document.querySelectorAll('.future-humid')
+
+      data.list.forEach(item => {
+        const daysDate = new Date(item.dt * 1000)
+        if (daysDate.getHours() === 11) {
+          fiveDayTemp.push(item.main.temp)
+          let fiveDayIconUrl = `https://openweathermap.org/img/wn/${item.weather[0].icon}.png`
+          fiveDayIcon.push(fiveDayIconUrl)
+          fiveDayWind.push(item.wind.speed)
+          fiveDayHumid.push(item.main.humidity)
+        }
+      })
+      futureTemp.forEach((temp, i) => {
+        temp.innerHTML = `${Math.floor(fiveDayTemp[i])} <span>&#176;</span>`
+      })
+      futureIcon.forEach((icon, i) => {
+        icon.setAttribute('src', fiveDayIcon[i])
+      })
+      for (let i = 0; i < 5; i++) {
+        const nextDate = currentDate.add(i + 1, 'day');
+        fiveDayDate.push(nextDate.format('ddd MM/DD'));
+      }
+      futureDate.forEach((day, i) => {
+        day.textContent = fiveDayDate[i]
+      })
+      futureWind.forEach((value, i) => {
+        value.innerHTML = `<i class="fa-solid fa-wind"></i> ${Math.floor(fiveDayWind[i])} mph`
+      })
+      futureHumid.forEach((value, i) => {
+        value.innerHTML = `RH: ${Math.floor(fiveDayHumid[i])} <i class="fa-solid fa-percent"></i> `
+      })
+
+      loader.classList.add('hidden')
+      resultSection.classList.remove('hidden')
+    })
+}
 
 function toggleSearchMenu() {
   recentSearchList.classList.toggle('active')
   renderLocalStorage()
 }
-function removeRecentSearch(e){
-  if (e.target.nodeName === 'SPAN') {
-    e.target.parentElement.remove()
-  }
-  saveLocalStorage()
-}
+
 function addRecentSearch() {
   const createHistoryItem = document.createElement('li')
   const removeSearchItem = document.createElement('span')
